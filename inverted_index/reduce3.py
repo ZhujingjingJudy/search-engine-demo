6d4fd76286192d3357129284b3896503efb83504
lukka10 <shdanli@umich.edu>
2023-04-13 14:51:02 -0400

    reduce3

--- inverted_index/reduce3.py
+++ inverted_index/reduce3.py
@@ -6,23 +6,26 @@ import sys
 import itertools
 import collections
 
+
 def reduce_one_group(key, group):
     """Reduce one group."""
     group = list(group)
     doc_appearence = collections.defaultdict(int)
     for line in group:
-        doc_id, tf = line.partition("\t")[2]
+        doc_id, tf = line.partition("\t")[2].split()
         doc_appearence[doc_id] += 1
     nk = 0
-    for key, value in doc_appearence.items():
+
+    for dict_items in doc_appearence.items():
         nk += 1
     for line in group:
-        doc_id, tf = line.partition("\t")[2]
+        doc_id, tf = line.partition("\t")[2].split()
         print(f"{key}\t{doc_id} {tf} {nk}")
 
 
 def keyfunc(line):
     """Return the key from a TAB-delimited key-value pair."""
+    # print(line.partition("\t")[0])
     return line.partition("\t")[0]
 
 
