3c0f50fdc3b523ea0ca6244b6594192af20fc7e7
ZhujingjingJudy <zhujudy@umich.edu>
2023-04-13 13:38:07 -0400

    map2.py

---
+++ inverted_index/map2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Map 2. Calculate term frequency"""
+import sys
+import re
+
+for line in sys.stdin:
+    text = line.partition("\t")[2]
+    doc_id = line.partition("\t")[0]
+    text = text.split("[")[1]
+    text = text.split("]")[0]
+    text = re.sub(r"[,']+"," ",text)
+    list_word = text.split()
+    length = len(list_word)
+    for i in range(length):
+        print(f"{doc_id} {list_word[i]}\t1")
