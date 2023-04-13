e89dcc6732271fdc14e289935f34117a78665ce5
ZhujingjingJudy <zhujudy@umich.edu>
2023-04-13 11:34:21 -0400

    map0&reduce0

--- example/reduce.py
+++ example/reduce.py
@@ -1,27 +1,27 @@
-"""Reduce 0."""
-
+#!/usr/bin/env python3
+"""Word count reducer."""
 import sys
 import itertools
+
 
 def main():
     """Divide sorted lines into groups that share a key."""
     for key, group in itertools.groupby(sys.stdin, keyfunc):
         reduce_one_group(key, group)
 
+
 def keyfunc(line):
     """Return the key from a TAB-delimited key-value pair."""
     return line.partition("\t")[0]
 
+
 def reduce_one_group(key, group):
     """Reduce one group."""
-    doc_count = 0
+    word_count = 0
     for line in group:
         count = line.partition("\t")[2]
-        doc_count += int(count)
-    print(f"document number\t{doc_count}")
-
-
-
+        word_count += int(count)
+    print(f"{key} {word_count}")
 
 
 if __name__ == "__main__":
--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
 """Map 0."""
+import sys
+import csv
+
+for line in csv.reader(sys.stdin):
+    # doc_id, doc_ti, doc_bo = line
+    print(f"one_doc\t1")
